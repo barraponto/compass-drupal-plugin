976dea5665d7a3e8e23c25c975378889bbb66b7a
Capi /. Etheriel <barraponto@gmail.com>
2010-11-12 00:21:23 -0200

    updated the manifest for release

--- templates/zen/manifest.rb
+++ templates/zen/manifest.rb
@@ -1,14 +1,16 @@
 # Make sure you list all the project template files here in the manifest.
 stylesheet 'zen-base.scss', :media => 'screen, projection', :to => "#{options[:project_name]}.scss"
-file 'template.php', :erb => true
+stylesheet 'zen-base-rtl.scss', :media => 'screen, projection', :to => "#{options[:project_name]}-rtl.scss"
 
-description "Zen Base Theme for Drupal"
+description "Zen Layout for Drupal Zen Theme"
 
 help %Q{
-  This plugin sets a STARTERKIT subtheme in your Compass project
+  This plugin starts a Zen layout stylesheet in your Compass project
 }
 
 welcome_message %Q{
-  The STARTERKIT has been renamed to the Project name.
-  All files have been renamed and edited accordingly.
+  The Zen layout stylesheet has been created.
+  You need to add it to YOURTHEME.info in order to load it into Drupal.
+  If either zen-fixed.css or zen-liquid.css are declared on YOURTHEME.info,
+  you should comment it to avoid CSS conflicts.
 }
