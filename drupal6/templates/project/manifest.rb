f213b50309d1253b5abb9ebf6b0fc4bf816121b1
Capi Etheriel <barraponto@gmail.com>
2012-03-08 10:31:03 -0300

    revamped core extension help output

--- drupal6/templates/project/manifest.rb
+++ drupal6/templates/project/manifest.rb
@@ -7,15 +7,19 @@ file '_drupal-base.scss.erb', :to => "_#{options[:project_name] || File.basename
 
 file 'drupal.scss.erb', :to => "#{options[:project_name] || File.basename(Compass.configuration.project_path)}.scss", :erb => true, :like => :stylesheet
 
-description "Drupal layout and skin stylesheets for object-oriented CSS development"
+description "Core-based Drupal theme with template css"
 
 help %Q{
-  This plugin starts Drupal layout and skin CSS files to speed up theme development
+  This plugin generates a template Drupal theme based on Core templates.
 }
 
 welcome_message %Q{
-  Hello and welcome to Drupal Compass Plugin.
-  This is a blank theme meant for skilled users only.
-  If you're looking for something fun and easy to use, try the Zen or Fusion variants.
-  You must enable relative assets in config.rb in order to use theme images in the css.
+Welcome to Drupal Compass Plugin.
+This is a blank theme meant for skilled users only.
+If you're looking for something fun and easy to use, try the Zen or Fusion variants.
+
+It implements the Template Approach in SASS template stylesheets.
+See http://www.chapterthree.com/blog/nica_lorber/design_drupal_template_approach
+
+* You MUST enable relative assets in config.rb in order to use images in the css.
 }
