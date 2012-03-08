fb9744938d9cba27fc21c3cec3446cced79db661
Capi Etheriel <barraponto@gmail.com>
2012-03-08 10:26:50 -0300

    added assets, revamped extension's help output

--- drupal6/templates/zen/manifest.rb
+++ drupal6/templates/zen/manifest.rb
@@ -1,18 +1,14 @@
-# Make sure you list all the project template files here in the manifest.
+# ERB processed Drupal theme files
 file 'zen.info.erb', :to => "#{options[:project_name] || File.basename(Compass.configuration.project_path)}.info", :erb => true
 file 'template.php.erb', :to => "template.php", :erb => true
 file 'theme-settings.php.erb', :to => "theme-settings.php", :erb => true
-
+# ERB processed SASS files
 file 'zen.scss.erb', :to => "#{options[:project_name] || File.basename(Compass.configuration.project_path)}.scss", :erb => true, :like => :stylesheet
-
 file 'zen-rtl.scss.erb', :to => "#{options[:project_name] || File.basename(Compass.configuration.project_path)}-rtl.scss", :erb => true, :like => :stylesheet
-
 file '_zen-base.scss.erb', :to => "_#{options[:project_name] || File.basename(Compass.configuration.project_path)}-base.scss", :erb => true, :like => :stylesheet
-
 file '../project/_skin.scss.erb', :to => "_#{options[:project_name] || File.basename(Compass.configuration.project_path)}-skin.scss", :erb => true, :like => :stylesheet
-
 file '_zen-skin.scss.erb', :to => "_#{options[:project_name] || File.basename(Compass.configuration.project_path)}-zen-skin.scss", :erb => true, :like => :stylesheet
-
+# STARTERKIT CSS files (that will eventually move to SCSS)
 file 'css/block-editing.css', :to => 'block-editing.css', :like => :css
 file 'css/block-editing-rtl.css', :to => 'block-editing-rtl.css', :like => :css
 file 'css/blocks.css', :to => 'blocks.css', :like => :css
@@ -43,10 +39,14 @@ file 'css/tabs.css', :to => 'tabs.css', :like => :css
 file 'css/tabs-rtl.css', :to => 'tabs-rtl.css', :like => :css
 file 'css/views-styles.css', :to => 'views-styles.css', :like => :css
 file 'css/wireframes.css', :to => 'wireframes.css', :like => :css
-
+# STARTERKIT built-in documentation
 file 'css/README.txt', :to => 'README.txt', :like => :css
 file 'js/README.txt', :to => 'README.txt', :like => :javascript
 file 'templates/README.txt'
+# STARTERKIT theme assets
+file 'favicon.ico'
+file 'screenshot.png'
+file 'logo.png'
 
 description "Zen Layout for Drupal Zen Theme"
 
@@ -55,9 +55,12 @@ help %Q{
 }
 
 welcome_message %Q{
-  The Zen layout stylesheet has been created.
-  You need to add it to YOURTHEME.info in order to load it into Drupal.
-  If either zen-fixed.css or zen-liquid.css are declared on YOURTHEME.info,
-  you should comment it to avoid CSS conflicts.
-  You must enable relative assets in config.rb in order to use theme images in the css.
+Welcome to Drupal Compass Plugin.
+This is a powerful and beginner-friendly theme based on Zen.
+
+It implements the Template Approach in SASS template stylesheets.
+See http://www.chapterthree.com/blog/nica_lorber/design_drupal_template_approach
+
+* You MUST download the Zen theme and put it in your sites/all/themes folder.
+* You MUST also enable relative assets in config.rb in order to use images in the css.
 }
